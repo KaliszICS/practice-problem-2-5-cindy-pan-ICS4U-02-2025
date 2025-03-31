e145dc1efc2704bb9ad5badf4446e192a8c796c8
cindy pan <cindy.pan@student.tdsb.on.ca>
2025-03-31 03:56:01 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,16 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static void insertionSort(char[] characters) {
+		for (int i = 1; i < characters.length; i++) {
+			// compare to all the numbers before
+			char key = characters[i];
+			int index = i - 1;
+			while (index >= 0 && characters[index] > key) { //check for -1 first
+				characters[index + 1] = characters[index];
+				index--;
+			}
+			characters[index + 1] = key;
+		}
 	}
 
 
